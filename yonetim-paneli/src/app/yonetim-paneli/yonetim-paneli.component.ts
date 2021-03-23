183b17454502e4b40536bb78ff28341f79e2ba3b
MeteHanBicer <67206744+MeteHanBicer@users.noreply.github.com>
2021-03-23 15:56:43 +0300

    commit v4

--- yonetim-paneli/src/app/yonetim-paneli/yonetim-paneli.component.ts
+++ yonetim-paneli/src/app/yonetim-paneli/yonetim-paneli.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./yonetim-paneli.component.css']
 })
 export class YonetimPaneliComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
   }
-
+  kiralamailanlariadet=1
+  kiralamailanlarihektar=0.829943;
+  kiralananArazilerAdet=1;
+  kiralananArazilerHektar=0.829943;
+  satisIlanlariAdet=4;
+  satisIlanlariHektar=1.697608;
+  satilanArazilerAdet=4;
+  satilanArazilerHektar=1.697608;
+  netGelirHesabiAdet=5;
+  netGelirHesabiHektar=2.527551;
+  kiymetTakdiriAdet=2;
+  kiymetTakdiriHektar=1.235468;
 }
