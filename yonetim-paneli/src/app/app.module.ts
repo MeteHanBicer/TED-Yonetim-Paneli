cb7a7af0f866b21d1689ba9ceb10319f5ee85961
MeteHanBicer <67206744+MeteHanBicer@users.noreply.github.com>
2021-03-24 10:31:40 +0300

    commit v6 card-bar-chart

--- yonetim-paneli/src/app/app.module.ts
+++ yonetim-paneli/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YonetimPaneliComponent } from './yonetim-paneli/yonetim-paneli.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { YonetimPaneliComponent } from './yonetim-paneli/yonetim-paneli.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- yonetim-paneli/src/app/yonetim-paneli/yonetim-paneli.component.ts
+++ yonetim-paneli/src/app/yonetim-paneli/yonetim-paneli.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+const SAMPLE_BARCHART_DATA:any[] = [
+  // { data: [2016,2017,2019,2020,2021],label:'Q3 Sales'},
+  { data: [1,1,4,4,5,2],label:'Başvuru Sayısı'},
+  { data: [0.829943,0.829943,1.697608,1.697608,2.527551,1.235468],label:'Başvuru Alanı'}
+];
+const SAMPLE_BARCHART_LABELS : string[] = [ 'Kiralama İlanları','Kiralanan Araziler','Satış İlanları','Satılan Araziler','Net Gelir Hesabı','Kıymet Takdiri'];
 @Component({
   selector: 'app-yonetim-paneli',
   templateUrl: './yonetim-paneli.component.html',
@@ -8,11 +13,17 @@ import { Component, Input, OnInit } from '@angular/core';
 export class YonetimPaneliComponent implements OnInit {
   
   constructor() { }
-  
+  public barChartData : any[] = SAMPLE_BARCHART_DATA;
+  public barChartLabels:string[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = 'bar';
+  public barChartLegend = false;
+  public barChartOptions:any = {
+    scaleShowVerticalLines : false,
+    resonsive : true
+  };
   ngOnInit() {
     
   }
- 
   kiralamailanlariadet=1;
   kiralamailanlarihektar=0.829943;
   kiralananArazilerAdet=1;
@@ -38,12 +49,15 @@ export class YonetimPaneliComponent implements OnInit {
   geneladetgelen:number;
   genelhektargelen:number;
   genelbaslikgelen:string;
-
 alert(geneladet:number,genelhektar:number,ad:string)
 {
 this.geneladetgelen=geneladet;
 this.genelhektargelen=genelhektar;
 this.genelbaslikgelen=ad;
 }
+  }
 
-}
+
+
+
+
